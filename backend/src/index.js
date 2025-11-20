502d1f7be54188d1429a77ccd2d0acbfb365064f
unknown <dawidans714@gmail.com>
2025-11-20 13:02:13 +0100

    Server start bugs fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import app from "./app.js";
 
 dotenv.config({
     path: './.env'
@@ -19,7 +20,7 @@ const startServer = async () => {
                 ${process.env.PORT}`)
         })
     } catch (error) {
-        console.log("MongoDB connection failed!", err);
+        console.log("MongoDB connection failed!", error);
     }
 }
 
