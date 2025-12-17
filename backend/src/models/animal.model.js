8d6bafba225226ebb47adddf1367dada00c96913
jocker18291 <dawidans714@gmail.com>
2025-12-17 16:07:47 +0100

    Animal model added

---
+++ backend/src/models/animal.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const animalSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    species: {
+        type: String,
+        required: true
+    },
+    owners: [{
+        type: Schema.Types.ObjectID,
+        ref: 'User'
+    }],
+    primaryVet: {
+        type: Schema.Types.ObjectID,
+        ref: 'User'
+    },
+    lastVisit: {
+        type: Date
+    }
+}, {
+    timestamps: true
+})
+
+export const Animal = mongoose.model("Animal", animalSchema)
