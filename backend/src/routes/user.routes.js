097fe05f931c3530328d409e8c637223e80bf27a
jocker18291 <dawidans714@gmail.com>
2026-01-12 22:37:32 +0100

    Add simple APIs routes (register, login, logout)

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,38 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user.model');
+import { Router } from 'express'
+import { loginUser, logoutuser, registerUser } from '../controllers/user.controller.js';
+const router = Router();
 
-router.post('/register', async (req, res) => {
-  try {
-    const { login, password, firstName, lastName, address } = req.body;
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/logout').post(logoutuser)
 
-    const userExists = await User.findOne({ email: login }); // Mapujemy login na email w modelu
-    if (userExists) {
-      return res.status(400).json({ message: 'Login jest już zajęty' });
-    }
-
-    const user = await User.create({
-      email: login,
-      password: password, 
-      firstName,
-      lastName,
-      address,
-      role: 'owner'
-    });
-
-    res.status(201).json({ message: 'Rejestracja pomyślna', userId: user._id });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.get('/vets', async (req, res) => {
-  try {
-    const vets = await User.find({ role: 'vet' }).select('-password');
-    res.json(vets);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-module.exports = router;
+export default router;
